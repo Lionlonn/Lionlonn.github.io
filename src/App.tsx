dd0835e4c660de4bc07a33557f58ac0485b12c09
AndreyTech <lionlonn14@gmail.com>
2025-06-04 19:45:15 +0300

    two commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import WebApp from '@twa-dev/sdk';
-
+import { CCarousel, CCarouselItem, CImage } from '@coreui/react'
 function App() {
   useEffect(() => {
     WebApp.ready(); // Сообщаем Telegram, что мы готовы
@@ -14,8 +14,17 @@ function App() {
 
   return (
     <div style={{ padding: 20 }}>
-      <h1>Привет из Telegram Mini App!</h1>
-      <p>Это React-приложение без бэкенда.</p>
+       <CCarousel>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src="./images/School_21.jpg" alt="slide 1" />
+        </CCarouselItem>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src="./images/Крутушка.jpg" alt="slide 2" />
+        </CCarouselItem>
+        <CCarouselItem>
+          <CImage className="d-block w-100" src="./images/котикк.jpg" alt="slide 3" />
+        </CCarouselItem>
+      </CCarousel>
     </div>
   );
 }
