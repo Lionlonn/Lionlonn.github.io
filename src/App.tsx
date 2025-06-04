6f9dc173675b3585ea4f050731dbbf38f08ec619
AndreyTech <lionlonn14@gmail.com>
2025-06-04 19:28:25 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
+  useEffect(() => {
+    WebApp.ready(); // Сообщаем Telegram, что мы готовы
+
+    WebApp.MainButton.setText('Нажми меня!');
+    WebApp.MainButton.onClick(() => {
+      WebApp.showAlert('Спасибо, ты нажал кнопку!');
+    });
+    WebApp.MainButton.show();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h1>Привет из Telegram Mini App!</h1>
+      <p>Это React-приложение без бэкенда.</p>
     </div>
   );
 }
